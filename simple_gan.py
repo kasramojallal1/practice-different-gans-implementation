bd4c65ff07dd6d27c29a23ccc3c41df689d0736a
Kasra Mojallal <kasramojallal1@gmail.com>
2022-08-23 11:06:50 +0430

    simple_gan was added

---
+++ simple_gan.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.datasets as datasets
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+from torch.utils.tensorboard import SummaryWriter
+
+
+class Discriminator(nn.Module):
+    def __init__(self, img_dim):
+        super().__init__()
+        self.disc = nn.Sequential(
+            nn.Linear(img_dim, 128),
+            nn.LeakyReLU(0.1),
+            nn.Linear(128, 1),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, x):
+        return self.disc(x)
+
+
+class Generator(nn.Module):
+    def __init__(self, z_dim, img_dim):
+        super().__init__()
+        self.gen = nn.Sequential(
+            nn.Linear(z_dim, 256),
+            nn.LeakyReLU(0.1),
+            nn.Linear(256, img_dim),
+            nn.Tanh(),
+        )
+
+    def forward(self, x):
+        return self.gen(x)
+
